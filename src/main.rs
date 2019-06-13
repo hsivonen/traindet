82936fda2d40cb23bab9d5dfeae8dbef2e043d44
Henri Sivonen <hsivonen@hsivonen.fi>
2019-06-13 13:02:00 +0300

    Add license header.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// Copyright 2019 Mozilla Foundation. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use bzip2::bufread::BzDecoder;
 use detector_char_classes::*;
 use detone::IterDecomposeVietnamese;
